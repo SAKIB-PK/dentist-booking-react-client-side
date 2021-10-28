a1d967459178b43cf2f1dd8a3264dc503eb2ad0d
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-10-28 15:17:28 +0600

    react env file add

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ3eXHhMcjHc_7ZvwLCTrLEaDNl1vGR7g",
-    authDomain: "dentistcare-authentication.firebaseapp.com",
-    projectId: "dentistcare-authentication",
-    storageBucket: "dentistcare-authentication.appspot.com",
-    messagingSenderId: "527343543718",
-    appId: "1:527343543718:web:b88985a6671142f877db67"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
   export default firebaseConfig
